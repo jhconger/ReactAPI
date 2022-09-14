215488b89bddce96e18d99f2966b6d985c521e5a
jhconger <jhconger@gmail.com>
2022-09-13 20:52:28 -0500

    random cat fact api call and return

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import Axios from 'axios';
+import React from 'react';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Axios = require('axios')
+    const [joke, setJoke] = useState("")
+
+//axios
+    const callApi = () => {
+        Axios.get("https://catfact.ninja/fact").then(
+            (response) => {
+                console.log(response);
+                setJoke(response.data.fact)
+            }
+        );
+    };
+
+    // function callApi() {
+    //   alert('Button was pressed!');
+    //     fetch('https://catfact.ninja/fact', { method: 'GET' })
+    //         .then(data => data.json())
+    //         // Parsing the data into a JavaScript object
+    //         .then(json => alert(JSON.stringify(json)))
+    //   // Displaying the stringified data in an alert popup
+    //   }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={callApi}>Call API</button>
+                {joke}
+            </header>
+        </div>
+    );
 }
 
 export default App;
